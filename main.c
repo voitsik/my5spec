acea149483139d46ac8afac2dfb7dfc526c50026
Petr Voytsik <voitsik@asc.rssi.ru>
2015-06-17 19:13:24 +0300

    Now compute dynamic spectra for all IFs.
    Result for each IF is written into a separate file called OUTFILE_n,
    where n is IF number, started with 01.

--- main.c
+++ main.c
@@ -19,6 +19,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include <complex.h>
 #include <fftw3.h>
 #include <mark5access.h>
@@ -33,9 +34,10 @@ struct fft_data_t {
     unsigned n_if;
 };
 
-static void fft_data_init(struct fft_data_t *fft_data, unsigned n_sp_chann, unsigned n_if)
+static int fft_data_init(struct fft_data_t *fft_data, unsigned n_sp_chann, unsigned n_if)
 {
     unsigned i;
+    int ret = 0;
 
     fft_data->n_if = n_if;
     fft_data->n_sp_chann = n_sp_chann;
@@ -54,6 +56,7 @@ static void fft_data_init(struct fft_data_t *fft_data, unsigned n_sp_chann, unsi
                                                   fft_data->zdata[i], FFTW_MEASURE);
     }
 
+    return ret;
 }
 
 static void fft_data_free(struct fft_data_t *fft_data)
@@ -65,7 +68,7 @@ static void fft_data_free(struct fft_data_t *fft_data)
         free(fft_data->spec[i]);
         fftwf_free(fft_data->zdata[i]);
     }
-
+    fftwf_cleanup();
     free(fft_data->plan);
     free(fft_data->spec);
     free(fft_data->data);
@@ -73,43 +76,77 @@ static void fft_data_free(struct fft_data_t *fft_data)
 
 }
 
-static int spec(const char *filename, const char *format, int nchan, 
-                double aver_time, double total_time, int if_num)
+/*
+static int mark5_stream_set_time_offset(struct mark5_stream *ms, 
+                                        double offset)
+{
+    int mjd, sec, ret = 0;
+    double ns;
+
+    if(!ms){
+        return -1;
+    }
+
+    ret = mark5_stream_get_sample_time(ms, &mjd, &sec, &ns);
+    if(ret == -1){
+        fprintf(stderr, "Error in mark5_stream_get_sample_time\n");
+
+        return ret;
+    }
+
+    return 0;
+}
+*/
+
+static int spec(const char *input_filename, const char *format, 
+                int nchan, double aver_time, double total_time, double offset,
+                const char *out_filename_base)
 {
     struct mark5_stream *ms;
     struct fft_data_t fft_data;
-    long long offset = 0;
     int nint;
     double real_step;
     int i, j, k;
     int c;  /* Iteration over spectral channels */
     int step_num = 0;  /* Number of time steps */
+    FILE **out_files;
+    char *out_filename;
 
-    ms = new_mark5_stream_absorb(new_mark5_stream_file(filename, offset),
+    ms = new_mark5_stream_absorb(new_mark5_stream_file(input_filename, 0LL),
                                  new_mark5_format_generic_from_string(format));
-    
     if(!ms){
-        fprintf(stderr, "Error: problem opening %s\n", filename);
+        fprintf(stderr, "Error: problem opening %s\n", input_filename);
 
         return EXIT_FAILURE;
     }
 
-    /*mark5_stream_print(ms);*/
+    /* Prepare output files */
+    out_filename = (char *)malloc(strlen(out_filename_base) * sizeof(char) + 4);
+    out_files = (FILE **)malloc(ms->nchan * sizeof(FILE *));
+    for(i = 0; i < ms->nchan; ++i){
+        sprintf(out_filename, "%s_%02d", out_filename_base, i+1);
+        out_files[i] = fopen(out_filename, "w");
+        if(!out_files[i]){
+            perror("Could not open output file");
+
+            return EXIT_FAILURE;
+        }
+    }
+    free(out_filename);
 
     nint = aver_time * ms->samprate / (2 * nchan);
-    fprintf(stderr, "nint = %d\n", nint);
+    printf("nint = %d\n", nint);
     real_step = (double)(nint * 2 * nchan) / (double)ms->samprate;
-    fprintf(stderr, "Real time step = %lf ms\n", real_step * 1e3);
+    printf("Real time step = %lf ms\n", real_step * 1e3);
     step_num = (int)(total_time / real_step);
-    fprintf(stderr, "Number of time steps = %d\n", step_num);
+    printf("Number of time steps = %d\n", step_num);
 
     /* Prepare data arrays */
     fft_data_init(&fft_data, nchan, ms->nchan);
 
     for(k = 0; k < step_num; ++k){
         /* Zero spec */
-        /*for(i = 0; i < ms->nchan; ++i)*/
-        i = if_num;
+        for(i = 0; i < ms->nchan; ++i)
             for(c = 0; c < nchan; ++c)
                 fft_data.spec[i][c] = 0.0;
 
@@ -126,8 +163,7 @@ static int spec(const char *filename, const char *format, int nchan,
                 fprintf(stderr, "Error: problem with data decoding\n");
                 break;
             }
-            /*for(i = 0; i < ms->nchan; ++i){*/
-            i = if_num;
+            for(i = 0; i < ms->nchan; ++i){
                 fftwf_execute(fft_data.plan[i]);
                 for(c = 0; c < nchan; ++c){
 
@@ -135,16 +171,22 @@ static int spec(const char *filename, const char *format, int nchan,
                     im = cimag(fft_data.zdata[i][c]);
                     fft_data.spec[i][c] += (re*re + im*im) / (double)(2*nchan);
                 }
-            /*}*/
+            }
         }
 
-        for(c = 0; c < nchan; ++c){
-            printf("%lf ", fft_data.spec[if_num][c] / (double)nint);
+        for(i = 0; i < ms->nchan; i++){
+            for(c = 0; c < nchan; ++c){
+                fprintf(out_files[i], "%lf ", fft_data.spec[i][c] / (double)nint);
+            }
+            fputc('\n', out_files[i]);
         }
-        putchar('\n');
 
     }
 
+    /* Free resources */
+    for(i = 0; i < ms->nchan; ++i)
+        fclose(out_files[i]);
+    free(out_files);
     fft_data_free(&fft_data);
     delete_mark5_stream(ms);
 
@@ -153,15 +195,17 @@ static int spec(const char *filename, const char *format, int nchan,
 
 static void usage(const char *prog_name)
 {
-    printf("Usage: %s [-a aver_time] [-n nchan] [-l time_limit] [-o offset] INPUT_FILE FORMAT OUTPUT_FILE \n\n", 
+    printf("Usage: %s [-a aver_time] [-n nchan] [-l time_limit] [-o offset] INFILE FORMAT OUTFILE \n\n", 
             prog_name);
-    printf("INPUT_FILE - the name of the input file\n");
-    printf("FORMAT     - mark5access data format in form <FORMAT>-<Mbps>-<nchan>-<nbit>\n");
+    printf("INFILE    - the name of the input file\n");
+    printf("FORMAT    - mark5access data format in form <FORMAT>-<Mbps>-<nchan>-<nbit>\n");
+    printf("OUTFILE   - basename for the output files.\n\
+Output files will be called 'OUTFILE_n', where n is IF number\n");
     printf("\noptional arguments:\n");
-    printf("-a aver_time  - approximate integration time per spectrum in milliseconds (1 ms)\n");
-    printf("-n nchan      - number of spectral channels (128)\n");
-    printf("-l time_limit - total time in seconds\n");
-    printf("-o offset     - offset in seconds from the file beginnig (0)\n");
+    printf("  -a aver_time  - approximate integration time per spectrum in milliseconds (1 ms)\n");
+    printf("  -n nchan      - number of spectral channels (128)\n");
+    printf("  -l time_limit - total time in seconds\n");
+    printf("  -o offset     - offset in seconds from the file beginnig (0)\n");
 }
 
 int main(int argc, char *argv[])
@@ -218,12 +262,13 @@ int main(int argc, char *argv[])
         }
     }
 
-    if(optind >= argc - 1){
+    if(optind >= argc - 2){
         usage(argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    ret = spec(argv[optind], argv[optind+1], nchan, aver_time, total_time, 1);
+    ret = spec(argv[optind], argv[optind+1], nchan, aver_time, total_time, offset,
+               argv[optind+2]);
 
     return ret;
 }
